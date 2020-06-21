44277356f4712def09a4c6f700e4d01e9d66b780
117503445 <t117503445@gmail.com>
2020-06-21 13:28:25 +0800

    修复 获取用户信息 API

--- src/main/java/com/wizzstudio/aplmu/security/rest/UserRestController.java
+++ src/main/java/com/wizzstudio/aplmu/security/rest/UserRestController.java
@@ -1,6 +1,9 @@
 package com.wizzstudio.aplmu.security.rest;
 
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +12,20 @@ import com.wizzstudio.aplmu.security.service.UserService;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = {"用户信息"})
 public class UserRestController {
 
-   private final UserService userService;
+    private final UserService userService;
 
-   public UserRestController(UserService userService) {
-      this.userService = userService;
-   }
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
 
-   @GetMapping("/user")
-   public ResponseEntity<User> getActualUser() {
-      return ResponseEntity.ok(userService.getUserWithAuthorities().get());
-   }
+    @Secured("ROLE_USER")
+    @GetMapping("/user")
+
+    public ResponseEntity<User> getActualUser() {
+        var a = userService.getUserWithAuthorities();
+        return a.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>((User) null, HttpStatus.NOT_FOUND));
+    }
 }

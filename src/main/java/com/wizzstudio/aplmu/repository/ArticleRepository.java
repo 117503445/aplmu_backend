6282aad49e1505f45350918663b1e680fa66c322
117503445 <t117503445@gmail.com>
2020-06-22 20:32:44 +0800

    添加 文章 Swagger注解
    0.1.4-SNAPSHOT

--- src/main/java/com/wizzstudio/aplmu/repository/ArticleRepository.java
+++ src/main/java/com/wizzstudio/aplmu/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package com.wizzstudio.aplmu.repository;
 
 import com.wizzstudio.aplmu.entity.Article;
+import io.swagger.annotations.Api;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@Api(tags = {"文章"})
 @RepositoryRestResource(collectionResourceRel = "article", path = "article")
 public interface ArticleRepository extends CrudRepository<Article, Integer> {
 

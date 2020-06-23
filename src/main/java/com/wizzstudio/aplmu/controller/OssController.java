058c9734a32646c9238331f61c47c5fc5d953f98
117503445 <t117503445@gmail.com>
2020-06-23 11:01:59 +0800

    优化 OSS 控制器

--- src/main/java/com/wizzstudio/aplmu/controller/OssController.java
+++ src/main/java/com/wizzstudio/aplmu/controller/OssController.java
@@ -1,44 +1,30 @@
 package com.wizzstudio.aplmu.controller;
-import java.io.*;
-import java.net.URI;
-import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.sql.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import io.swagger.annotations.Api;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
-
-//import org.junit.Assert;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.sf.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 @Api(tags = {"OSS上传"})
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api/oss")
 public class OssController {
     @Value("${alioss.accessId}")
     private String accessId;
@@ -49,18 +35,18 @@ public class OssController {
     @Value("${alioss.bucket}")
     private String bucket;
 
-
-    @GetMapping(path = "oss", produces = "application/json")
+    @ApiOperation("获取某个用户的上传凭证")
+    @Secured("ROLE_USER")
+    @GetMapping(path = "user", produces = "application/json")
     public @ResponseBody
-    String getJson() {
-        System.out.println("getJson");
-
-
+    String getJsonUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
 
         String host = "http://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
-        String callbackUrl = "https://aplmu.backend.117503445.top";
-        String dir = "user-dir-prefix/"; // 用户上传文件时指定的前缀。
+        String callbackUrl = "https://aplmu.backend.117503445.top/api/oss/callback";
+        String dir = String.format("user/%s", authentication.getName()); // 用户上传文件时指定的前缀。
         OSSClient client = new OSSClient(endpoint, accessId, accessKey);
         long expireTime = 30;
         long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
@@ -79,7 +65,7 @@ public class OssController {
         String encodedPolicy = BinaryUtil.toBase64String(binaryData);
         String postSignature = client.calculatePostSignature(postPolicy);
 
-        Map<String, String> respMap = new LinkedHashMap<String, String>();
+        Map<String, String> respMap = new LinkedHashMap<>();
         respMap.put("accessid", accessId);
         respMap.put("policy", encodedPolicy);
         respMap.put("signature", postSignature);
@@ -96,9 +82,15 @@ public class OssController {
         String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
         respMap.put("callback", base64CallbackBody);
 
-        JSONObject ja1 = JSONObject.fromObject(respMap);
-        return  ja1.toString();
+        JSONObject jsonObject = JSONObject.fromObject(respMap);
+        return jsonObject.toString();
 
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping(path = "callback", produces = "application/json")
+    public void callback() {
+        System.out.println("oss callback");
     }
 
 }

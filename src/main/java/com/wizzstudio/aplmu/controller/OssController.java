b2ed3d554bd182ca96a30d55083aaa311bfc6f8d
117503445 <t117503445@gmail.com>
2020-06-23 19:04:35 +0800

    改进 OSS
    0.4.5-SNAPSHOT

--- src/main/java/com/wizzstudio/aplmu/controller/OssController.java
+++ src/main/java/com/wizzstudio/aplmu/controller/OssController.java
@@ -1,6 +1,7 @@
 package com.wizzstudio.aplmu.controller;
 
-import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
@@ -57,7 +58,7 @@ public class OssController {
     String getJsonArticle(@RequestParam(name = "articleId", required = true) Integer articleId) throws CustomException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         assert authentication != null;
-
+        authentication.getAuthorities();
         var article = articleRepository.findById(articleId);
         if (article.isEmpty()) {
             throw new CustomException(HttpStatus.NOT_FOUND, "文章不存在");
@@ -78,7 +79,7 @@ public class OssController {
         String host = "http://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
         String callbackUrl = "https://aplmu.backend.117503445.top/api/oss/callback";
-        OSSClient client = new OSSClient(endpoint, accessId, accessKey);
+        OSS client = new OSSClientBuilder().build(endpoint, accessId, accessKey);
         long expireTime = 30;
         long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
         Date expiration = new Date(expireEndTime);
@@ -109,12 +110,14 @@ public class OssController {
         respMap.put("callback", base64CallbackBody);
 
         JSONObject jsonObject = JSONObject.fromObject(respMap);
+        client.shutdown();
         return jsonObject.toString();
     }
 
     @GetMapping(path = "callback", produces = "application/json")
-    public void callback() {
+    public String callback() {
         System.out.println("oss callback");
+        return "success callback";
     }
 
 }

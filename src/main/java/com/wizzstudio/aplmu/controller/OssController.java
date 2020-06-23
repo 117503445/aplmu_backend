37fd5d93b5a167a67ce737c7de69dd3f50c40e80
117503445 <t117503445@gmail.com>
2020-06-23 16:07:45 +0800

    添加 获取文章上传凭证 API
    0.4.4-SNAPSHOT

--- src/main/java/com/wizzstudio/aplmu/controller/OssController.java
+++ src/main/java/com/wizzstudio/aplmu/controller/OssController.java
@@ -4,19 +4,20 @@ import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
+import com.wizzstudio.aplmu.error.CustomException;
+import com.wizzstudio.aplmu.repository.ArticleRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -26,6 +27,9 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/api/oss")
 public class OssController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @Value("${alioss.accessId}")
     private String accessId;
     @Value("${alioss.accessKey}")
@@ -42,11 +46,38 @@ public class OssController {
     String getJsonUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         assert authentication != null;
+        String dir = String.format("user/%s/", authentication.getName()); // 用户上传文件时指定的前缀。
+        return getUploadJson(dir);
+    }
+
+    @ApiOperation("获取某个文章的上传凭证")
+    @Secured("ROLE_USER")
+    @GetMapping(path = "article", produces = "application/json")
+    public @ResponseBody
+    String getJsonArticle(@RequestParam(name = "articleId", required = true) Integer articleId) throws CustomException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
+
+        var article = articleRepository.findById(articleId);
+        if (article.isEmpty()) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "文章不存在");
+        }
+        //todo 管理员也可以进行修改
+        if (article.get().getAuthor().equals(authentication.getName())) {
+            throw new CustomException(HttpStatus.UNAUTHORIZED, "不可以编辑他人的文章");
+        }
+
+        String dir = String.format("article/%s/", article.get().getId()); // 用户上传文件时指定的前缀。
+        return getUploadJson(dir);
+    }
+
+
+    private String getUploadJson(String dir) {
+
 
         String host = "http://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
         String callbackUrl = "https://aplmu.backend.117503445.top/api/oss/callback";
-        String dir = String.format("user/%s/", authentication.getName()); // 用户上传文件时指定的前缀。
         OSSClient client = new OSSClient(endpoint, accessId, accessKey);
         long expireTime = 30;
         long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
@@ -56,12 +87,7 @@ public class OssController {
         policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
 
         String postPolicy = client.generatePostPolicy(expiration, policyConds);
-        byte[] binaryData = new byte[0];
-        try {
-            binaryData = postPolicy.getBytes("utf-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        byte[] binaryData = postPolicy.getBytes(StandardCharsets.UTF_8);
         String encodedPolicy = BinaryUtil.toBase64String(binaryData);
         String postSignature = client.calculatePostSignature(postPolicy);
 
@@ -84,9 +110,8 @@ public class OssController {
 
         JSONObject jsonObject = JSONObject.fromObject(respMap);
         return jsonObject.toString();
-
     }
-    
+
     @GetMapping(path = "callback", produces = "application/json")
     public void callback() {
         System.out.println("oss callback");

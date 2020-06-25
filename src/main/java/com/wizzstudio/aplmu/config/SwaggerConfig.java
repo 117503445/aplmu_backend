0ea7e686bb29c1a4d97551047e2bea28b9e5473e
117503445 <t117503445@gmail.com>
2020-06-25 10:56:55 +0800

    修复 swagger JWT失效的问题
    0.5.8-SNAPSHOT

--- src/main/java/com/wizzstudio/aplmu/config/SwaggerConfig.java
+++ src/main/java/com/wizzstudio/aplmu/config/SwaggerConfig.java
@@ -5,17 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Import(SpringDataRestConfiguration.class)
@@ -33,25 +31,40 @@ public class SwaggerConfig {
      */
     @Value("${app.build.time}")
     private String buildTime;
+
     @Bean
     public Docket createRestApi() {
-
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name("Authorization").description("JWT Authorization").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
-
 
         return new Docket(DocumentationType.SWAGGER_2).host(swaggerHost)
                 .pathMapping("/")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build().globalOperationParameters(pars).apiInfo(new ApiInfoBuilder()
+                .build().securitySchemes(Collections.singletonList(securitySchemes())).securityContexts(Collections.singletonList(securityContext())).apiInfo(new ApiInfoBuilder()
                         .title("aplmu")
                         .description("aplmu 后端")
                         .version(String.format("%s %s UTC+0", version, buildTime))
                         .contact(new Contact("Qht", "https://www.117503445.top", "t117503445@gmail.com"))
                         .build());
+    }
+
+    private SecurityScheme securitySchemes() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(
+                new SecurityReference("Authorization", authorizationScopes));
     }
 }

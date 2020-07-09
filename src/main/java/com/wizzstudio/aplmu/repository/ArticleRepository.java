4b3cc1c7e9bacdb8544e5dfd7c042de228e3faeb
117503445 <t117503445@gmail.com>
2020-07-09 09:19:38 +0800

    修复 Repository 的暴露
    0.6.4-SNAPSHOT

--- src/main/java/com/wizzstudio/aplmu/repository/ArticleRepository.java
+++ src/main/java/com/wizzstudio/aplmu/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import com.wizzstudio.aplmu.entity.ArticleEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Integer> {
     Page<ArticleEntity> findAllByAuthorID(Pageable pageable, long authorId);
 }
--- src/main/java/com/wizzstudio/aplmu/repository/CourseRepository.java
+++ src/main/java/com/wizzstudio/aplmu/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.wizzstudio.aplmu.repository;
 
 import com.wizzstudio.aplmu.entity.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
 }
